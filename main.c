c4e02decbc7d51f29fc058d154a1676b10f8b948
Jeong Arm <kjwonmail@gmail.com>
2022-03-11 13:29:28 +0900

    Increase batch size and count, Use nanosec

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define BATCH_SIZE 524288
+#define BATCH_COUNT 200
+
 void do_plus(float* left, float* right, float* result, size_t count);
 void do_minus(float* left, float* right, float* result, size_t count);
 void do_multiply(float* left, float* right, float* result, size_t count);
@@ -14,9 +17,19 @@ float rand_float32(void) {
   return f;
 }
 
+void timespec_diff(struct timespec* start, struct timespec* end,
+                   struct timespec* result) {
+  result->tv_sec = end->tv_sec - start->tv_sec;
+  result->tv_nsec = end->tv_nsec - start->tv_nsec;
+  if (result->tv_nsec < 0) {
+    result->tv_sec -= 1;
+    result->tv_nsec += 1000000000;
+  }
+}
+
 int main(int argc, char *argv[]) {
-    float left[4096], right[4096];
-    float result[4096];
+    float left[BATCH_SIZE], right[BATCH_SIZE];
+    float result[BATCH_SIZE];
 
     if (argc > 1) {
         srand(atoi(argv[1]));
@@ -24,37 +37,44 @@ int main(int argc, char *argv[]) {
         srand(0);
     }
 
-    for (int i = 0; i < 4096; i++) {
+    for (int i = 0; i < BATCH_SIZE; i++) {
         left[i] = rand_float32();
         right[i] = rand_float32();
     }
 
-    struct timespec start, end;
-    double elapsed;
+    struct timespec start, end, elapsed;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    do_plus(left, right, result, 4096);
+    for (int i = 0; i < BATCH_COUNT; i++) {
+        do_plus(left, right, result, BATCH_SIZE);
+    }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Plus: %1.14f\n", elapsed);
+    timespec_diff(&start, &end, &elapsed);
+    printf("Plus: %2lu.%09lu\n", elapsed.tv_sec, elapsed.tv_nsec);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    do_plus(left, right, result, 4096);
+    for (int i = 0; i < BATCH_COUNT; i++) {
+        do_minus(left, right, result, BATCH_SIZE);
+    }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Minus: %1.14f\n", elapsed);
+    timespec_diff(&start, &end, &elapsed);
+    printf("Minus: %2lu.%09lu\n", elapsed.tv_sec, elapsed.tv_nsec);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    do_multiply(left, right, result, 4096);
+    for (int i = 0; i < BATCH_COUNT; i++) {
+        do_multiply(left, right, result, BATCH_SIZE);
+    }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Multiply: %1.14f\n", elapsed);
+    timespec_diff(&start, &end, &elapsed);
+    printf("Multiply: %2lu.%09lu\n", elapsed.tv_sec, elapsed.tv_nsec);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    do_divide(left, right, result, 4096);
+    for (int i = 0; i < BATCH_COUNT; i++) {
+        do_divide(left, right, result, BATCH_SIZE);
+    }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Divide: %1.14f\n", elapsed);
+    timespec_diff(&start, &end, &elapsed);
+    printf("Divide: %2lu.%09lu\n", elapsed.tv_sec, elapsed.tv_nsec);
 
     return 0;
 }
