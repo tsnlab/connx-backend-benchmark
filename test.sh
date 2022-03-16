5cab907ce43dc79d5840f435330040ee588407df
Jeong Arm <kjwonmail@gmail.com>
2022-03-16 14:29:37 +0900

    Restyle test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,28 @@
-if [[ $# -eq 1 && $1 == 'xnnpack' ]]; then
-    CFLAGS="XNNPACK/build/local/libXNNPACK.a XNNPACK/build/local/cpuinfo/libcpuinfo.a XNNPACK/build/local/clog/libclog.a XNNPACK/build/local/pthreadpool/libpthreadpool.a -lpthread -I XNNPACK/include/ -I XNNPACK/build/local/pthreadpool-source/include/ -Wno-deprecated-declarations"
+#!/bin/bash
+set -eo pipefail
 
-    echo '* CPU: -O0'
-    gcc -O0 src/main.c src/xnnpack.c $CFLAGS && ./a.out
+backend="${1:-cpu}"
 
-    echo '* CPU: -O3'
-    gcc -O3 src/main.c src/xnnpack.c $CFLAGS && ./a.out
+case $backend in
+    "xnnpack")
+        CFLAGS="XNNPACK/build/local/libXNNPACK.a XNNPACK/build/local/cpuinfo/libcpuinfo.a XNNPACK/build/local/clog/libclog.a XNNPACK/build/local/pthreadpool/libpthreadpool.a -lpthread -I XNNPACK/include/ -I XNNPACK/build/local/pthreadpool-source/include/ -Wno-deprecated-declarations"
 
-    rm -f a.out
-else
-    echo '* CPU: -O0'
-    gcc -O0 src/main.c src/cpu.c && ./a.out
+        echo '* XNNPACK: -O0'
+        gcc -O0 src/main.c src/xnnpack.c $CFLAGS && ./a.out
 
-    echo '* CPU: -O3'
-    gcc -O3 src/main.c src/cpu.c && ./a.out
+        echo '* XNNPACK: -O3'
+        gcc -O3 src/main.c src/xnnpack.c $CFLAGS && ./a.out
 
-    rm -f a.out
-fi
+        rm -f a.out
+        ;;
+
+    "cpu")
+        echo '* CPU: -O0'
+        gcc -O0 src/main.c src/cpu.c && ./a.out
+
+        echo '* CPU: -O3'
+        gcc -O3 src/main.c src/cpu.c && ./a.out
+
+        rm -f a.out
+        ;;
+esac
